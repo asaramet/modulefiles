56e244533924795eae5e29c47fa65c2f6af1c23b
Alexandru Saramet <alexandru@saramet.de>
2023-03-27 20:12:55 +0200

    new versions

---
+++ starccm+/2302_install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+module load compiler/intel/2022.2.1
+module load mpi/impi/2021.7.1
+
+VERSION="18.02.008"
+V_LONG="2302"
+BASE_DIR="/opt/bwhpc/es/cae/starccm+"
+SRC_DIR="${BASE_DIR}/src"
+BUILD_DIR="${BASE_DIR}/build"
+
+[[ ! -d ${BUILD_DIR} ]] && mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+tar zxvf "${SRC_DIR}/${V_LONG}/STAR-CCM+${VERSION}_01_linux-x86_64.tar.gz"
+cd starccm+_${VERSION}
+
+export _JAVA_OPTIONS="-Xmx2G"
+./STAR-CCM+${VERSION}_01_linux-x86_64-2.17_gnu11.2.sh -i console -DINSTALLDIR=$BASE_DIR -DINSTALLFLEX=false -DADDSYSTEMPATH=false -DNODOC=true
+
+cd ${BASE_DIR}/${VERSION}/STAR-CCM+${VERSION}/mpi/intel/2021.6/
+mv linux-x86_64 linux-x86_64.org
+ln -s /software/all/toolkit/Intel_OneAPI/mpi/2021.7.1 linux-x86_64
