9c6b2b72b6920e3ae831edb598c5c61fa1aa57f2
Alexandru Saramet <alexandru@saramet.de>
2023-04-21 15:19:09 +0200

    add starccm+ 2302

--- starccm+/2210_install.sh
+++ starccm+/2210_install.sh
@@ -3,8 +3,8 @@
 module load compiler/intel/2022.2.1
 module load mpi/impi/2021.7.1
 
-VERSION="18.02.010"
-V_LONG="2302"
+VERSION="17.06.008"
+V_LONG="2210"
 BASE_DIR="/opt/bwhpc/es/cae/starccm+"
 SRC_DIR="${BASE_DIR}/src"
 BUILD_DIR="${BASE_DIR}/build"
@@ -15,7 +15,7 @@ tar zxvf "${SRC_DIR}/${V_LONG}/STAR-CCM+${VERSION}_01_linux-x86_64.tar.gz"
 cd starccm+_${VERSION}
 
 export _JAVA_OPTIONS="-Xmx2G"
-./STAR-CCM+${VERSION}_01_linux-x86_64-2.17_gnu11.2.sh -i console -DINSTALLDIR=$BASE_DIR -DINSTALLFLEX=false -DADDSYSTEMPATH=false -DNODOC=true
+./STAR-CCM+${VERSION}_01_linux-x86_64-2.17_gnu9.2.sh -i console -DINSTALLDIR=$BASE_DIR -DINSTALLFLEX=false -DADDSYSTEMPATH=false -DNODOC=true
 
 cd ${BASE_DIR}/${VERSION}/STAR-CCM+${VERSION}/mpi/intel/2021.6/
 mv linux-x86_64 linux-x86_64.org
